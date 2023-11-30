2d44a8c8f500357d5a112b21753e8ad33f57650b
Trygve Aaberge <trygve.aaberge@autodesk.com>
2023-11-30 15:35:56 +0100

    Allow floating panel url to be overridden

--- src/LeftSide.tsx
+++ src/LeftSide.tsx
@@ -1,13 +1,29 @@
 import { Forma } from "forma-embedded-view-sdk/auto"
+import { Vec3 } from "forma-embedded-view-sdk/dist/internal/scene/design-tool"
 import { useCallback } from "preact/hooks"
+
+function getFloatingPanelUrl(polygon: Vec3[] | undefined) {
+  const overriddenUrl = new URLSearchParams(window.location.search).get(
+    "floating-panel-url",
+  )
+  const url = new URL(
+    overriddenUrl ??
+      "https://trygve-aaberge-adsk.github.io/extendathon-terrain-editing/",
+  )
+
+  if (polygon != null) {
+    const query = new URLSearchParams(url.search)
+    query.set("polygon", JSON.stringify(polygon))
+    url.search = query.toString()
+  }
+
+  return url.toString()
+}
 
 export default function LeftSide() {
   const selectPolygon = useCallback(() => {
     Forma.designTool.getPolygon().then((polygon) => {
-      const polygonQuery = JSON.stringify(polygon)
-      const url = `https://trygve-aaberge-adsk.github.io/extendathon-terrain-editing/?polygon=${encodeURIComponent(
-        polygonQuery,
-      )}`
+      const url = getFloatingPanelUrl(polygon)
       Forma.openFloatingPanel({
         embeddedViewId: "floating-panel",
         url,
